e97a46f905d01f5f7082da41cdf643fc628ec9c9
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-02 08:53:42 -0500

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F5F5F7',
+        'secondary': '#A1A1AA',
+        'tertiary': '#46A4F6',
+        'background': '#111215'
+      }
+    },
   },
   plugins: [],
 };

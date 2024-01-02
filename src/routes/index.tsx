e5280ae08d9f65bde47a4702f345e44e8942db53
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-02 08:57:24 -0500

    Fix formatter with new rules

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,26 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik'
+import type { DocumentHead } from '@builder.io/qwik-city'
 
 export default component$(() => {
   return (
     <>
       <h1>Hi 👋</h1>
-      <p class="font-mono">
+      <p class='font-mono'>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </p>
     </>
-  );
-});
+  )
+})
 
 export const head: DocumentHead = {
-  title: "Sebastian Garcia",
+  title: 'Sebastian Garcia',
   meta: [
     {
-      name: "Sobre mi",
-      content: "Desarrollador de software, entusiasta del open source y la infraestructura del software",
+      name: 'Sobre mi',
+      content:
+        'Desarrollador de software, entusiasta del open source y la infraestructura del software',
     },
   ],
-};
+}

b559b8ce1bdf9d9dcd36bfdc642bbcbe1ddd851a
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-02 08:57:12 -0500

    Add new rules to prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,8 @@
+/**@type {import("prettier").Config} */
 export default {
   plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  semi: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
 }

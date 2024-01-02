084dbf942f2155721ee32c32e1ce2ea97676a781
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-02 09:26:06 -0500

    Add main container

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -13,5 +13,9 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
 }
 
 export default component$(() => {
-  return <Slot />
+  return (
+    <main class='container mx-auto max-w-4xl'>
+      <Slot />
+    </main>
+  )
 })

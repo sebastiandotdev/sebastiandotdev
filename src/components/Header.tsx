27962670bd20ad72c1e3b0d7af86ae0bebf55765
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-07 00:40:23 -0500

    Add size icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,35 @@
 import { component$ } from '@builder.io/qwik'
+import { Link } from '@builder.io/qwik-city'
+import { Email, Github, LinkedIn } from './icons'
 
 export const Header = component$(() => {
-  return <div></div>
+  return (
+    <>
+      <nav class='container pt-6'>
+        <Link
+          href='https://github.com/seb-24'
+          target='_blank'
+          class='flex items-center gap-2 text-secondary font-onest-regular cursor-pointer hover:opacity-90 hover:underline hover:transition-all'
+        >
+          <Github />
+          Github
+        </Link>
+        <Link
+          href='https://www.linkedin.com/in/castrogarciajs/'
+          target='_blank'
+          class='flex items-center gap-2 text-secondary font-onest-regular py-3 cursor-pointer hover:opacity-90 hover:underline hover:transition-all'
+        >
+          <LinkedIn />
+          Linkein
+        </Link>
+        <Link
+          href='mailto:johancs.mm@gmail.com'
+          class='flex items-center gap-2 text-secondary font-onest-regular cursor-pointer hover:opacity-90 hover:underline hover:transition-all'
+        >
+          <Email />
+          Correo Electrinico
+        </Link>
+      </nav>
+    </>
+  )
 })
--- src/components/icons/Available.tsx
+++ src/components/icons/Available.tsx
@@ -6,8 +6,8 @@ export const Available = component$(() => {
       <svg
         xmlns='http://www.w3.org/2000/svg'
         class='icon icon-tabler icon-tabler-point-filled animate-pulse'
-        width='24'
-        height='24'
+        width='26'
+        height='26'
         viewBox='0 0 24 24'
         stroke-width='2'
         stroke='currentColor'

faaf81828450a4e07e8985763cf543d195455edd
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-06 23:19:15 -0500

    Rename ext to json

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-/**@type {import("prettier").Config} */
-export default {
-  plugins: ['prettier-plugin-tailwindcss'],
-  singleQuote: true,
-  semi: false,
-  jsxSingleQuote: true,
-  printWidth: 80,
-}

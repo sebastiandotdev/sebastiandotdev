68caa8dd71db9dbfaa6bc93637a46e484bd88b1d
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-07 00:39:49 -0500

    Add constant experience

---
+++ src/lib/const.ts
@@ -0,0 +1,9 @@
+export const EXPERIENCIE = [
+    {
+      date: "Actualmente...",
+      title: "Desarrollador Frontend - Bogota",
+      description:
+        "Trabajando en la creacion de domiplace.",
+      link: "https://prueba.domiplace.com",
+    },
+]

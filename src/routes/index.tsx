322b78d58cea88770607bc0db72221084e6449d8
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-07 00:40:35 -0500

    Add section experiencie

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
-import { Location, Available, Github, Email } from '@/components/icons'
-import { LinkedIn } from '@/components/icons/LinkedIn'
+import { Header } from '@/components/Header'
+import { Location, Available, Arrow } from '@/components/icons'
+import { Briefcase } from '@/components/icons/Briefcase'
+import { EXPERIENCIE } from '@/lib/const'
 import { component$ } from '@builder.io/qwik'
-import type { DocumentHead } from '@builder.io/qwik-city'
+import { type DocumentHead, Link } from '@builder.io/qwik-city'
 
 export default component$(() => {
   return (
@@ -11,13 +13,13 @@ export default component$(() => {
           <h1 class='font-onest-bold text-5xl font-bold'>
             Hey, Soy Sebastián García
           </h1>
-          <p class='font-onest-regular text-secondary pt-4 pb-5 pl-2'>
+          <p class='font-onest-regular text-secondary pt-4 pb-5'>
             Desarrollador de software
           </p>
-          <p class='font-onest-regular text-secondary pl-2 text-base text-balance'>
+          <p class='font-onest-regular text-secondary text-base text-balance'>
             +2 Años de experiencia.{' '}
-            <b class='text-yellow-500'>De Colombia, Cesar.</b> Especializado en
-            el desarrollo de aplicaciones únicas. <br />
+            <b class='text-tertiary'>De Colombia, Cesar.</b> Especializado en el
+            desarrollo de aplicaciones únicas. <br />
             Amante de la infraestructura que mejoran la experiencia del
             desarrollo.
           </p>
@@ -27,28 +29,44 @@ export default component$(() => {
             <Location />
             Cesar, Valledupar
           </p>
-          <p class='flex items-center gap-2 text-secondary font-onest-regular'>
+          <p class='flex items-center gap-1 text-secondary font-onest-regular'>
             <Available />
             Disponible para trabajar
           </p>
         </article>
       </section>
-      <nav class='container pt-6'>
-        <ul class='py-1'>
-          <li class='flex items-center gap-2 text-secondary font-onest-regular'>
-            <Github />
-            Github
-          </li>
-          <li class='flex items-center gap-2 text-secondary font-onest-regular py-3'>
-            <LinkedIn />
-            Linkein
-          </li>
-          <li class='flex items-center gap-2 text-secondary font-onest-regular'>
-            <Email />
-            Correo Electrinico
-          </li>
-        </ul>
-      </nav>
+      <Header />
+      <section class='pt-28'>
+        <h2 class='text-3xl font-semibold mb-6 flex gap-x-3 items-center font-onest-regular text-primary'>
+          <Briefcase class='size-8' />
+          Experiencia laboral
+        </h2>
+        <ol class='relative border-s border-gray-200 ml-3'>
+          {EXPERIENCIE.map((experiencie) => (
+            <li class='mb-10 ms-4'>
+              <div class='absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-indigo-100 bg-white'></div>
+              <time class='mb-1 text-sm font-normal font-onest-regular text-primary'>
+                {experiencie.date}
+              </time>
+              <h3 class='text-lg font-semibold text-tertiary font-onest-regular mt-2'>
+                {experiencie.title}
+              </h3>
+              <p class='mb-4 text-base font-normal text-pretty text-secondary font-onest-regular'>
+                {experiencie.description}
+              </p>
+              {experiencie.link && (
+                <Link
+                  href={experiencie.link}
+                  target='_blank'
+                  class='bg-background border dark:border-white/10 border-gray-300 rounded-full inline-flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 md:px-4 text-xs md:text-base transition hover:scale-110 font-onest-regular'
+                >
+                  Saber más... <Arrow />
+                </Link>
+              )}
+            </li>
+          ))}
+        </ol>
+      </section>
     </>
   )
 })

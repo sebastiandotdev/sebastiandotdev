e6ad84d7848615f63b6bee29d3a396b36fe4da67
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-07 22:47:00 -0500

    Add content experiencie

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -3,7 +3,19 @@ export const EXPERIENCIE = [
       date: "Actualmente...",
       title: "Desarrollador Frontend - Bogota",
       description:
-        "Trabajando en la creacion de domiplace.",
-      link: "https://prueba.domiplace.com",
+        "En mi rol como Desarrollador Frontend en Diens, formo parte integral de un equipo dedicado a la creación y mantenimiento de una aplicación avanzada para la gestión de pedidos con tecnologías de vanguardia.",
+      link: "https://info.domiplace.com",
+    },
+    {
+      date: 'Noviembre 2023',
+      title: 'Full Stack Free Lance - Ecuador',
+      description: 'Durante un período de tiempo, tuve el privilegio de desempeñar el rol de Full Stack Freelance en Ecuador, contribuyendo al éxito de Nestlé a través del desarrollo y mantenimiento de una aplicación integral de promociones. En esta posición, lideré la creación y evolución tanto del frontend como del backend, asegurando una experiencia de usuario optimizada y un rendimiento robusto del sistema.',
+      link: 'https://www.nestle-contigo.ec/promotangogamers'
+    },
+    {
+      date: "Noviembre 2022",
+      title: "Mentor Frontend - Valledupar",
+      description:
+        "Desempeñé el rol de docente en la academia Syntactic en Valledupar, impartiendo clases presenciales de React. En este rol docente, proporciono las bases necesarias en React para capacitar a estudiantes en el desarrollo y gestión efectiva de aplicaciones web.",
     },
 ]

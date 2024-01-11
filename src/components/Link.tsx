4e0386a32a7dc7972562d2d2d6c3505f16a8c633
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-10 20:51:36 -0500

    Add prop target

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -3,10 +3,12 @@ import { Link } from '@builder.io/qwik-city'
 import { LinkProps } from 'types/component'
 
 export const LinkItem = component$<LinkProps>((props) => {
+  const value = props.target ? '_blank' : ''
   return (
     <>
       <Link
         href={props.path}
+        target={value}
         class='flex items-center gap-2 text-secondary cursor-pointer hover:opacity-90  hover:transition-all py-2'
       >
         <Slot />

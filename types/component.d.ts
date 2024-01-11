317d49249a58c1ce029170578380e1aea0de4306
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-10 21:23:01 -0500

    Add test e2e component

--- types/component.d.ts
+++ types/component.d.ts
@@ -1,14 +1,14 @@
 /**
  * path is URL Link component
  * @example
- * 
+ *
  * <LinkItem path='URL'/>
- * 
+ *
  * @default
  * @type {boolean}
  * target
- * 
+ *
  * @example
  * <LinkItem target />
  */
-export type LinkProps = { path: string, target?: boolean }
+export type LinkProps = { path: string; target?: boolean }

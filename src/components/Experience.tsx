bad423c1750598303a59b6987583f43e78d5e28b
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-10 20:35:14 -0500

    Refactor font-family

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -10,20 +10,20 @@ export const Experience = component$(() => {
         {EXPERIENCIE.map((experiencie) => (
           <li class='mb-10 ms-4'>
             <div class='absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-indigo-100 bg-white'></div>
-            <time class='mb-1 text-sm font-normal font-onest-regular text-primary'>
+            <time class='mb-1 text-sm font-normal text-primary'>
               {experiencie.date}
             </time>
-            <h3 class='text-xl font-semibold text-tertiary font-onest-regular mt-2'>
+            <h3 class='text-xl font-semibold text-tertiary mt-2'>
               {experiencie.title}
             </h3>
-            <p class='mb-4 text-base font-normal text-pretty text-secondary font-onest-regular'>
+            <p class='mb-4 text-base font-normal text-pretty text-secondary'>
               {experiencie.description}
             </p>
             {experiencie.link && (
               <Link
                 href={experiencie.link}
                 target='_blank'
-                class='bg-background border dark:border-white/10 border-gray-300 rounded-full inline-flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 md:px-4 text-xs md:text-base transition hover:scale-110 font-onest-regular'
+                class='bg-background border dark:border-white/10 border-gray-300 rounded-full inline-flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 md:px-4 text-xs md:text-base transition hover:scale-110'
               >
                 Saber más... <Arrow />
               </Link>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,27 @@
 import { component$ } from '@builder.io/qwik'
-import { Link } from '@builder.io/qwik-city'
-import { Email, Github, LinkedIn } from './icons'
+import { LinkItem } from './Link'
+import { Code, Email, Github, LinkedIn } from './icons'
 
 export const Header = component$(() => {
   return (
     <>
       <nav class='container pt-6'>
-        <Link
-          href='https://github.com/seb-24'
-          target='_blank'
-          class='flex items-center gap-2 text-secondary font-onest-regular cursor-pointer hover:opacity-90  hover:transition-all'
-        >
+        <LinkItem path='https://github.com/seb-24'>
           <Github />
           <b class='hover:text-tertiary'>Github</b>
-        </Link>
-        <Link
-          href='https://www.linkedin.com/in/castrogarciajs/'
-          target='_blank'
-          class='flex items-center gap-2 text-secondary font-onest-regular py-3 cursor-pointer hover:opacity-90 hover:transition-all'
-        >
+        </LinkItem>
+        <LinkItem path='https://www.linkedin.com/in/castrogarciajs/'>
           <LinkedIn />
           <b class='hover:text-tertiary'>Linkedin</b>
-        </Link>
-        <Link
-          href='mailto:johancs.mm@gmail.com'
-          class='flex items-center gap-2 text-secondary font-onest-regular cursor-pointer hover:opacity-90 hover:transition-all'
-        >
+        </LinkItem>
+        <LinkItem path='mailto:johancs.mm@gmail.com'>
           <Email />
           <b class='hover:text-tertiary'>Correo Electrinico</b>
-        </Link>
+        </LinkItem>
+        <LinkItem path='#projects'>
+          <Code />
+          <b class='hover:text-tertiary'>Proyectos</b>
+        </LinkItem>
       </nav>
     </>
   )

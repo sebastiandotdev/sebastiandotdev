2089e2629bd98473b5c9ee4bb8789e680ac47e22
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-10 20:35:36 -0500

    Add new section projects

---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+import { component$, Slot } from '@builder.io/qwik'
+
+export const Title = component$(() => {
+  return (
+    <>
+      <h2 class='text-3xl font-semibold mb-6 flex gap-x-3 items-center text-primary'>
+        <Slot />
+      </h2>
+    </>
+  )
+})

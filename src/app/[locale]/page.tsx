6eeee505e1882d1983774d40c164b2be3e4e146e
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-14 20:48:16 -0500

    Add section projects

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,30 +1,32 @@
-import Experience from "@/components/experience";
-import Header from "@/components/header";
-import { Available, Briefcase, Location } from "@/components/icons";
-import Title from "@/components/title";
-import { useTranslations } from "next-intl";
+import Experience from '@/components/experience'
+import Header from '@/components/header'
+import { Available, Briefcase, Code, Location } from '@/components/icons'
+import Projects from '@/components/projects'
+import Title from '@/components/title'
+import { useTranslations } from 'next-intl'
 
 export default function Index() {
-  const t = useTranslations("Index");
-  const e = useTranslations("Title");
+  const t = useTranslations('Index')
+  const e = useTranslations('Title')
+  const m = useTranslations('Menu')
   return (
     <>
       <section>
-        <article className="flex flex-col items-start justify-start pt-44">
-          <h1 className="text-5xl font-bold">{t("title")}</h1>
-          <p className="text-secondary pt-4 pb-5">{t("profession")}</p>
-          <p className="text-secondary text-base text-balance">
-            {t("description")}
+        <article className='flex flex-col items-start justify-start pt-44'>
+          <h1 className='text-5xl font-bold'>{t('title')}</h1>
+          <p className='text-secondary pt-4 pb-5'>{t('profession')}</p>
+          <p className='text-secondary text-base text-balance'>
+            {t('description')}
           </p>
         </article>
-        <article className="pt-6 flex gap-8 items-center">
-          <p className="flex items-center gap-2 text-secondary">
+        <article className='pt-6 flex gap-8 items-center'>
+          <p className='flex items-center gap-2 text-secondary'>
             <Location />
             Cesar, Valledupar
           </p>
-          <p className="flex items-center gap-0.5 text-secondary">
+          <p className='flex items-center gap-0.5 text-secondary'>
             <Available />
-            {t("available")}
+            {t('available')}
           </p>
         </article>
       </section>
@@ -36,6 +38,13 @@ export default function Index() {
         </Title>
         <Experience />
       </section>
+      <section id='projects' className='pt-28'>
+        <Title>
+          <Code />
+          {m('projects')}
+        </Title>
+        <Projects />
+      </section>
     </>
-  );
+  )
 }

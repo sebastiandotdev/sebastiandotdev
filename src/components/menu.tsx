72515a15d36236530109d17ba26ed95b08bcaf90
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-20 21:22:45 -0500

    Add new icon navbar

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,19 +1,16 @@
-import Link from 'next/link'
 import { DarkMode } from './dark-mode'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from './ui/select'
+import { Colombia, Usa } from './icons'
 
 export default function Menu() {
   return (
     <header className='flex justify-end items-center pr-6 pt-6'>
       <div className='relative flex justify-center gap-6 items-center text-left'>
-        <a href='/en/'>Inglés</a>
-        <a href='/es/'>Español</a>
+        <a href='/en/'>
+          <Usa className='size-6' />
+        </a>
+        <a href='/es/'>
+          <Colombia className='size-6' />
+        </a>
         <DarkMode />
       </div>
     </header>

235a355721c219b18ad4660449b8b0ceaaf0c920
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-20 21:22:55 -0500

    Add FastAPI project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
-import { Github, Nextjs, Preact, Supabase, Tailwind } from './icons'
+import { FastAPI, Github, Nextjs, Preact, Supabase, Tailwind } from './icons'
 import { Code } from 'lucide-react'
 import Link from 'next/link'
 import { Badge } from './ui/badge'
+import { Carousel } from './carousel'
 
 export default function Projects() {
   const TAGS = {
@@ -13,28 +14,34 @@ export default function Projects() {
     },
     TAILWIND: {
       name: 'Tailwind CSS',
-      className: 'bg-[#003159] text-white',
+      className: 'bg-[#003159]',
       icon: Tailwind,
     },
     PREACT: {
       name: 'Preact.js',
-      className: 'bg-[#673ab8] text-white',
+      className: 'bg-[#673ab8]',
       icon: Preact,
     },
     SUPABASE: {
       name: 'Supabase',
-      className: 'bg-[#232323] text-white',
+      className: 'bg-[#232323]',
       icon: Supabase,
+    },
+    FASTAPI: {
+      name: 'FastAPI',
+      className: '',
+      icon: FastAPI,
     },
   }
   const PROJECTS = [
     {
       title: 'Nestle Promo - Participa por miles de premios',
-      description: 'Creado desde cero con Preact.js, Supabase y Tailwind CSS.',
+      description:
+        'Creado desde cero con FastAPI, Preact.js, Supabase y Tailwind CSS.',
       link: 'https://www.nestle-contigo.ec/promotangogamers',
       github: '',
       image: '/wepb/nestle.webp',
-      tags: [TAGS.PREACT, TAGS.TAILWIND, TAGS.SUPABASE],
+      tags: [TAGS.FASTAPI, TAGS.PREACT, TAGS.TAILWIND, TAGS.SUPABASE],
     },
     {
       title: 'Merch - Comercio para comprar el accesorio de tu mascota',
@@ -52,14 +59,16 @@ export default function Projects() {
           <h3 className='text-2xl font-semibold text-tertiary mb-2'>{title}</h3>
           <p className='text-lg mb-4 text-pretty'>{description}</p>
           <ul className='flex gap-x-2 flex-row mb-2'>
-            {tags.map((tag, j) => (
-              <li key={j}>
-                <Badge className={tag.className} variant='outline'>
-                  {<tag.icon className='size-6' />}
-                  <small className='px-2'>{tag.name}</small>
-                </Badge>
-              </li>
-            ))}
+            <Carousel>
+              {tags.map((tag, j) => (
+                <li key={j} className='shrink-0 snap-start'>
+                  <Badge className={tag.className} variant='outline'>
+                    {<tag.icon className='size-6' />}
+                    <small className='px-2'>{tag.name}</small>
+                  </Badge>
+                </li>
+              ))}
+            </Carousel>
           </ul>
           <Image
             loading='lazy'

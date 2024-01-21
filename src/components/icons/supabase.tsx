58209970b163440c21ad6be075187e47196eb023
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-20 21:23:22 -0500

    Fix atributtes jsx

--- src/components/icons/supabase.tsx
+++ src/components/icons/supabase.tsx
@@ -19,7 +19,7 @@ export const Supabase: SVGProps = (props) => {
       <path
         d='M63.708 110.284c-2.86 3.601-8.658 1.628-8.727-2.97l-1.007-67.251h45.22c8.19 0 12.758 9.46 7.665 15.874l-43.151 54.347Z'
         fill='url(#b)'
-        fill-opacity='.2'
+        fillOpacity='.2'
       />
       <path
         d='M45.317 2.071c2.86-3.601 8.657-1.628 8.726 2.97l.442 67.251H9.83c-8.19 0-12.759-9.46-7.665-15.875L45.317 2.072Z'
@@ -34,8 +34,8 @@ export const Supabase: SVGProps = (props) => {
           y2='71.829'
           gradientUnits='userSpaceOnUse'
         >
-          <stop stop-color='#249361' />
-          <stop offset='1' stop-color='#3ECF8E' />
+          <stop stopColor='#249361' />
+          <stop offset='1' stopColor='#3ECF8E' />
         </linearGradient>
         <linearGradient
           id='b'
@@ -46,7 +46,7 @@ export const Supabase: SVGProps = (props) => {
           gradientUnits='userSpaceOnUse'
         >
           <stop />
-          <stop offset='1' stop-opacity='0' />
+          <stop offset='1' stopOpacity='0' />
         </linearGradient>
       </defs>
     </svg>
--- src/components/icons/tailwind.tsx
+++ src/components/icons/tailwind.tsx
@@ -16,8 +16,8 @@ export const Tailwind: SVGProps = (props) => {
       >
         <defs>
           <linearGradient x1='-2.778%' y1='32%' x2='100%' y2='67.556%' id='a'>
-            <stop stop-color='#2298BD' offset='0%' />
-            <stop stop-color='#0ED7B5' offset='100%' />
+            <stop stopColor='#2298BD' offset='0%' />
+            <stop stopColor='#0ED7B5' offset='100%' />
           </linearGradient>
         </defs>
         <path

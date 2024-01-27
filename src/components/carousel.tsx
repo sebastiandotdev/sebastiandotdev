77f9f3bdc09c2d165f25bbe7d4fdef3167a29274
castrogarciajs <johancs.mm@gmail.com>
2024-01-27 17:37:26 -0500

    Add plugin autoplay

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,14 +1,29 @@
 'use client'
 
 import type { ReactNode } from 'react'
+import type { EmblaPluginType } from 'embla-carousel'
+import useAutoplay from 'embla-carousel-autoplay'
 import useEmblaCarousel from 'embla-carousel-react'
 
 type CarouselProps = {
   children: ReactNode
+  loop?: boolean
+  auto?: boolean
 }
 
-export const Carousel = ({ children }: CarouselProps) => {
-  const [emblaRef] = useEmblaCarousel()
+export const Carousel = ({
+  children,
+  loop = false,
+  auto = false,
+}: CarouselProps) => {
+  const autoplay = useAutoplay()
+  const plugins: EmblaPluginType[] = []
+
+  if (auto) {
+    plugins.push(autoplay)
+  }
+
+  const [emblaRef] = useEmblaCarousel({ loop }, plugins)
 
   return (
     <div className='overflow-hidden' ref={emblaRef}>

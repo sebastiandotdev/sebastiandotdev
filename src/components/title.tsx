d1253b3510369dfc2972fd5d5bf56680073b42e3
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-02-13 21:45:08 -0500

    Remove mb-8 to component title

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 export default function Title(props: { children: ReactNode }) {
   return (
     <>
-      <h2 className="text-3xl font-semibold mb-6 flex gap-x-3 items-center text-primary">
+      <h2 className="text-3xl font-semibold flex gap-x-3 items-center text-primary">
         {props.children}
       </h2>
     </>

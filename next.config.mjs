fa2db74db0559002c08b6b4ee6bf50f0ab24c7f4
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-02-13 21:39:08 -0500

    Add security hostname

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ import withNextIntl from 'next-intl/plugin'
 const locale = withNextIntl()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+}
 
 export default locale(nextConfig)

6247b55173c2da6a239544a6b54c4e967ac8e772
Sebastián García <johancs.mm@gmail.com>
2024-05-22 10:15:48 -0500

    Update styles for header and background components (#8)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -74,12 +74,24 @@ export default {
           from: { height: 'var(--kb-collapsible-content-height)' },
           to: { height: 0 },
         },
+        'spotlight': {
+          '0%': {
+            opacity: '0',
+            transform: 'translate(-72%,-62%) scale(.5)',
+          },
+
+          'to': {
+            opacity: '1',
+            transform: 'translate(-50%,-40%) scale(1)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'collapsible-down': 'collapsible-down 0.2s ease-out',
         'collapsible-up': 'collapsible-up 0.2s ease-out',
+        'spotlight': '2s ease .75s 1 forwards',
       },
     },
   },

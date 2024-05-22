ef48fb60d62807170b373c7bb12c41cc7dca2ebd
castrogarciajs <johancs.mm@gmail.com>
2024-05-22 12:35:35 -0500

    Create styles for header and footer components

---
+++ src/utils/constants.ts
@@ -0,0 +1,39 @@
+export const socialSite = [
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/',
+    },
+    {
+        name: 'Github',
+        url: 'https://github.com/castrogarciajs',
+    },
+ {
+        name: 'X',
+        url: 'https://twitter.com/mjs_sebastian',
+    },
+]
+
+export const linksNavbar = [
+    {
+        name: 'Resume',
+        url: '/',
+        type: 'Resume'
+    },
+    {
+        name: 'Github',
+        url: 'https://github.com/castrogarciajs',
+        type: 'Github'
+    },
+    {
+        name: 'Follow me on',
+        url: 'https://x.com/mjs_sebastian',
+        type: 'X'
+    },
+]
+
+export const contacts = [
+    {
+        name: 'Gmail',
+        url: 'mailto:castrogarcia.mjs@gmail.com'
+    },
+]

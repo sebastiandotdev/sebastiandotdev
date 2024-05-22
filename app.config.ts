fca85fb86c5b5c6eaac15c761b7edfe8b227971a
castrogarciajs <johancs.mm@gmail.com>
2024-05-21 22:49:01 -0500

    Add provider by default

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  server: {preset: 'deno-server'},
  vite: {
     resolve: {
       alias: {

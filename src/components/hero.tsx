5602197193d1d844cf9e41612aa8ea7b6ea54f12
castrogarciajs <johancs.mm@gmail.com>
2024-05-23 10:45:07 -0500

    Add ExperienceSection component

---
+++ src/components/hero.tsx
@@ -0,0 +1,10 @@
+export default function HeroSection() {
+  return (
+    <div class="h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <div class="mt-20 p-4 max-w-7xl mx-auto relative w-full pt-20 md:pt-0">
+        <h1 class="text-4xl md:text-7xl font-bold text-center dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 dark:bg-opacity-50">Sebastián García</h1>
+        <p class="mt-4 font-normal text-base max-w-lg text-center mx-auto">+2 Years of experience. From Colombia, Cesar. lover of infrastructure that improves the developer experience.</p>
+      </div>
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,15 @@
+import ExperienceSection from '@/components/experience'
 import Footer from '@/components/footer'
 import Header from '@/components/header'
+import HeroSection from '@/components/hero'
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <div class="h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
-          <div class="mt-20 p-4 max-w-7xl mx-auto relative w-full pt-20 md:pt-0">
-            <h1 class="text-4xl md:text-7xl font-bold text-center dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 dark:bg-opacity-50">Sebastián García</h1>
-            <p class="mt-4 font-normal text-base max-w-lg text-center mx-auto">+2 Years of experience. From Colombia, Cesar. lover of infrastructure that improves the developer experience.</p>
-          </div>
-        </div>
+        <HeroSection />
+        <ExperienceSection />
       </main>
       <Footer />
     </>

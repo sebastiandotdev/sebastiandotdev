21e1816f3eeea2567fb2a0a9363e62c224b04999
Sebastián García <johancs.mm@gmail.com>
2024-05-22 20:39:32 -0500

    Update fonts and styles background (#12)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import { linksNavbar } from '@/utils/constants'
 
 export default function Header() {
   const shouldRenderSocialIcon = 'X'
+  const shouldRenderResume = 'Resume'
   const [prevScroll, setPrevScroll] = createSignal(0)
   const [isVisible, setIsVisable] = createSignal(true)
 
@@ -22,7 +23,7 @@ export default function Header() {
     <header class={cn('flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-full [&:has(a)]:font-medium z-10 pr-2 pl-8 py-2 items-center justify-center space-x-4 transition-opacity', isVisible() ? 'bg-background opacity-100' : 'opacity-0 bg-transparent')}>
       <For each={linksNavbar}>
         {({ name, url, type }) => (
-          <a href={url} class={cn('relative items-center flex space-x-1', type === shouldRenderSocialIcon ? 'border text-sm font-medium relative px-4 py-2 rounded-full flex items-center gap-1' : '')}>
+          <a href={url} class={cn('relative items-center flex space-x-1 hover:underline', type === shouldRenderSocialIcon ? 'border text-sm font-medium relative px-4 py-2 rounded-full flex items-center gap-1 !no-underline hover:opacity-90 transition-opacity' : '')} download={type === shouldRenderResume ? 'Sebastian-Garcia' : 'Error-Download'}>
             {name}
             {type === shouldRenderSocialIcon && (
               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-brand-x inline">

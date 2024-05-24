96e3fcfa701efeb95e1213683279ea170b2b266a
castrogarciajs <johancs.mm@gmail.com>
2024-05-23 21:33:40 -0500

    Add new page contact and icons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
+import ContactSection from '@/components/contact'
 import ExperienceSection from '@/components/experience'
 import Footer from '@/components/footer'
 import Header from '@/components/header'
 import HeroSection from '@/components/hero'
+import FloatSocials from '@/components/socials'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main>
+      <FloatSocials />
+      <main class="relative">
         <HeroSection />
         <ExperienceSection />
+        <ContactSection />
       </main>
       <Footer />
     </>

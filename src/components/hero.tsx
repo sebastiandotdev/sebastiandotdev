05ebd182f603beb583e1ceeb15b0d558aaf31259
castrogarciajs <johancs.mm@gmail.com>
2024-08-03 22:30:11 -0500

    Update curriculum vitae

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
         <h1 class="text-4xl md:text-7xl font-bold text-center dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 dark:bg-opacity-50">Sebastián García</h1>
         <p class="mt-4 font-normal text-base max-w-lg text-center mx-auto">+2 Years of experience. From Colombia, Cesar. lover of infrastructure that improves the developer experience.</p>
         <div class="mt-8 gap-4 w-full flex justify-center max-w-lg mx-auto">
-          <a href="/curriculum.docx" class={cn(buttonVariants(), 'px-12')} download="Sebastian-Garcia">
+          <a href="/Curriculum-Sebastian.es.docx" class={cn(buttonVariants(), 'px-12')} download="Sebastian-Garcia">
             Resume
           </a>
 

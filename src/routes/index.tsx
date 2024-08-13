32d432a5cf87593847b8d4fceb28c044ec2f5c6c
Sebastián García <johancs.mm@gmail.com>
2024-08-13 14:46:05 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,12 @@ import Footer from '@/components/footer'
 import Header from '@/components/header'
 import HeroSection from '@/components/hero'
 import ProjectsSection from '@/components/projects'
-import FloatSocials from '@/components/socials'
 import UpHome from '@/components/up-home'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <FloatSocials />
       <UpHome />
       <main class="relative">
         <HeroSection />

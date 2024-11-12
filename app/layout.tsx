a7a1c732dccca4fa3c379806a9ba1fd240f4eb79
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-11 20:54:05 -0500

    Add Container component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 import OverlayMenu from '@/components/OverlayMenu'
+import { Container } from '@/styled-system/jsx'
 
 const onest = localFont({
   src: [
@@ -30,7 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={onest.className}>
         <OverlayMenu />
-        {children}
+        <Container marginTop="24">{children}</Container>
       </body>
     </html>
   )

e1bf588017a662171f329bff9e0ffc19a854ec9d
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-16 15:39:43 -0500

    Improve folder structure

---
+++ src/components/pages/posts.tsx
@@ -0,0 +1,11 @@
+import { Box, panda } from '@/styled-system/jsx'
+
+export default function Posts() {
+  return (
+    <Box marginTop="20">
+      <panda.h2 fontSize="2xl" fontWeight="bold" textWrap="balance" margin="0">
+        Posts
+      </panda.h2>
+    </Box>
+  )
+}
--- src/components/pages/work-experience.tsx
+++ src/components/pages/work-experience.tsx
@@ -1,13 +1,12 @@
 import { Box, Grid, panda } from '@/styled-system/jsx'
 import type { Work as WorkType } from '@/src/static/cv'
 import curriculumJSON from '@/src/static/curriculum.json' with { type: 'json' }
-import Work from './work'
+import Work from '@/src/components/work'
 
 export default function WorkExperience() {
   const works = curriculumJSON.work as unknown as WorkType[]
   return (
     <Box marginTop="44">
-      <h2></h2>
       <panda.h2 fontSize="2xl" fontWeight="bold" textWrap="balance" margin="0">
         Experiencia laboral
       </panda.h2>

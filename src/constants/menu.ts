9914da90b49428892c78dc578377ecf0b6835ff6
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-16 15:42:30 -0500

    Add `constants` folder

---
+++ src/constants/menu.ts
@@ -0,0 +1,40 @@
+import {
+  HomeIcon,
+  ProfileIcon,
+  TerminalIcon,
+  ShoppingBangIcon,
+  PhoneIcon,
+} from '@/src/icons'
+
+export const menus = [
+  {
+    id: 1,
+    name: 'Inicio',
+    path: '/',
+    Icon: HomeIcon,
+  },
+  {
+    id: 2,
+    path: '/',
+    name: 'Perfil',
+    Icon: ProfileIcon,
+  },
+  {
+    id: 3,
+    name: 'Proyectos',
+    path: '/',
+    Icon: TerminalIcon,
+  },
+  {
+    id: 4,
+    name: 'Productos',
+    path: '/',
+    Icon: ShoppingBangIcon,
+  },
+  {
+    id: 5,
+    name: 'Contacto',
+    path: '/',
+    Icon: PhoneIcon,
+  },
+]
---
+++ src/constants/social.ts
@@ -0,0 +1,22 @@
+import { GithubIcon, XIcon, LinkedInIcon } from '@/src/icons'
+
+export const social = [
+  {
+    id: 1,
+    name: 'X',
+    path: '/',
+    Icon: XIcon,
+  },
+  {
+    id: 2,
+    path: '/',
+    name: 'LinkedIn',
+    Icon: LinkedInIcon,
+  },
+  {
+    id: 3,
+    name: 'Github',
+    path: '/',
+    Icon: GithubIcon,
+  },
+]

7ab36be4a0e287089d66a837f523d65c1c975a5c
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-16 14:18:12 -0500

    Create `tooltip` component

---
+++ src/components/ui/tooltip.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { css, cx } from '@/styled-system/css'
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import * as React from 'react'
+
+const TooltipProvider = TooltipPrimitive.Provider
+
+const Tooltip = TooltipPrimitive.Root
+
+const TooltipTrigger = TooltipPrimitive.Trigger
+
+const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 5, ...props }, ref) => (
+  <TooltipPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={cx(
+      css({
+        z: '50',
+        overflow: 'hidden',
+        bg: 'white',
+        _dark: {
+          bg: 'neutral.950',
+        },
+        rounded: 'md',
+        userSelect: 'none',
+        willChange: 'transform, opacity',
+        px: '3',
+        py: '1.5',
+        fontSize: 'sm',
+        shadow: 'md',
+      }),
+      className,
+    )}
+    {...props}
+  />
+))
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }

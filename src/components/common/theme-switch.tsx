6a07c804784990ac6b7d69eb94bf88ef3296b108
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-20 22:32:49 -0500

    Improve `setTheme` in `theme-switch`

--- src/components/common/theme-switch.tsx
+++ src/components/common/theme-switch.tsx
@@ -3,18 +3,67 @@
 import { css } from '@/styled-system/css'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
+import { flushSync } from 'react-dom'
+
 import { MoonIcon, SunIcon } from '@/src/icons'
+
+type Theme = 'light' | 'dark' | 'system'
 
 function useThemeSwitch() {
   const [mounted, setMounted] = useState(false)
-  const { setTheme, resolvedTheme } = useTheme()
+  const { setTheme, resolvedTheme, theme: currentTheme } = useTheme()
 
   useEffect(() => {
     setMounted(true)
   }, [])
 
   const isDark = mounted && resolvedTheme === 'dark'
-  const toggleTheme = () => setTheme(isDark ? 'light' : 'dark')
+
+  const toggleTheme = (event: MouseEvent, theme: Theme) => {
+    const isAppearanceTransition =
+      // @ts-expect-error: Transition API
+      document.startViewTransition &&
+      !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+    if (!isAppearanceTransition || !event) {
+      setTheme(theme)
+      return
+    }
+
+    const x = event.clientX
+    const y = event.clientY
+
+    const endRadius = Math.hypot(
+      Math.max(x, window.innerWidth - x),
+      Math.max(y, window.innerHeight - y),
+    )
+
+    const transition = document.startViewTransition(() => {
+      flushSync(() => {
+        setTheme(theme)
+      })
+    })
+
+    transition.ready.then(() => {
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ]
+
+      document.documentElement.animate(
+        {
+          clipPath: isDark ? clipPath : [...clipPath].reverse(),
+        },
+        {
+          duration: 400,
+          easing: 'ease-in',
+          pseudoElement: isDark
+            ? '::view-transition-new(root)'
+            : '::view-transition-old(root)',
+        },
+      )
+    })
+  }
 
   return {
     toggleTheme,
@@ -47,7 +96,9 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      onClick={toggleTheme}
+      onClick={(e) =>
+        toggleTheme(e as unknown as MouseEvent, isDark ? 'light' : 'dark')
+      }
       className={css({
         cursor: 'pointer',
       })}

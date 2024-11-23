2700243296f3067572aa6b79a7385a5c2be1c2d5
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-23 17:06:29 -0500

    Remove imports `resend` for now

--- app/_actions/email.ts
+++ app/_actions/email.ts
@@ -1,26 +1,13 @@
 'use server'
 
-import process from 'node:process'
-import { Resend } from 'resend'
-import { EmailTemplate } from '../email-template'
-
 interface EmailForm {
   email: string
   username: string
   description: string
 }
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export async function sendEmail(formData: FormData) {
   const form = Object.fromEntries(formData.entries()) as unknown as EmailForm
 
-  const { data, error } = await resend.emails.send({
-    from: form.email,
-    to: ['sebastian@castrogarciajs.dev'],
-    subject: form.description,
-    react: EmailTemplate({ username: form.username }),
-  })
-
-  console.log({ data, error })
+  console.log(form)
 }

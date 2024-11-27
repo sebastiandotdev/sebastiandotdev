612377290d27dac946d287e46f3612927280221f
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-26 21:05:44 -0500

    Use `props` in <TagIcon />

--- src/icons/tag.tsx
+++ src/icons/tag.tsx
@@ -5,6 +5,7 @@ const Tag: FC<ComponentProps<'svg'>> = (props) => (
     width="1em"
     height="1em"
     viewBox="0 0 24 24"
+    {...props}
     fill="none"
     stroke="currentColor"
     strokeWidth="2"

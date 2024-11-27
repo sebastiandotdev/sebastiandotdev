bb3bbecb6fec2eeebd28c6c4f86554d186162f33
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-26 21:04:46 -0500

    Add projects open sources

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -1,5 +1,7 @@
-import { Box, HStack, panda } from '@/styled-system/jsx'
-import { Badge } from '@/src/components/recipes/badge'
+import { Box, Grid, HStack, panda } from '@/styled-system/jsx'
+import ProjectsList from '@/src/components/common/projects-list'
+import curriculumJSON from '@/src/static/curriculum.json' with { type: 'json' }
+import { Projects as ProjectType } from '@/src/static/cv'
 
 export default function Projects() {
   return (
@@ -13,10 +15,12 @@ export default function Projects() {
         >
           Proyectos
         </panda.h2>
-        <Badge bordered="warn" py="1">
-          Beta
-        </Badge>
       </HStack>
+      <Grid columns={{ base: 1, lg: 2 }} gap="6" mt="14">
+        {(curriculumJSON.projects as ProjectType[]).map((_, index) => (
+          <ProjectsList key={index} project={_} />
+        ))}
+      </Grid>
     </Box>
   )
 }

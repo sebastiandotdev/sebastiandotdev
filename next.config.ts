6a942024abc53da2f4571587473fbaf8fccbae11
sebastiandotdev <sebastiandotdev@gmail.com>
2024-12-07 10:43:36 -0500

    Add contentlayer2 dependecy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,3 @@
-import type { NextConfig } from 'next'
+import { withContentlayer } from 'next-contentlayer2'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-}
-
-export default nextConfig
+export default withContentlayer({})

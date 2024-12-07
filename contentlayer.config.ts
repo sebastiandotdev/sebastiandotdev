6e5d18f9a2b8c883a4c829a4f192b8605bca9702
sebastiandotdev <sebastiandotdev@gmail.com>
2024-12-07 10:43:36 -0500

    Add contentlayer2 dependecy

---
+++ contentlayer.config.ts
@@ -0,0 +1,30 @@
+import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
+
+const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      description: 'The title of the post',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      description: 'The date of the post',
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'posts',
+  documentTypes: [Post],
+})

b96809d56fde1f3b581c4deaecff399e242bf095
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-13 21:20:20 -0500

    Add config for posts

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -15,6 +15,11 @@ const Post = defineDocumentType(() => ({
       description: 'The date of the post',
       required: true,
     },
+    description: {
+      type: 'string',
+      description: 'The description of the post',
+      required: true,
+    },
   },
   computedFields: {
     url: {
--- src/components/common/posts-list.tsx
+++ src/components/common/posts-list.tsx
@@ -2,8 +2,10 @@ import { Box, Flex, HStack, Stack, panda } from '@/styled-system/jsx'
 import { Tag } from '@/src/components/recipes/tag'
 import Link from 'next/link'
 import { ChevronRightIcon, TagIcon } from '@/src/icons'
+import type { Post } from '@/.contentlayer/generated'
+import { format } from '@formkit/tempo'
 
-export default function BlogPosts() {
+export default function BlogPosts({ post }: { post: Post }) {
   return (
     <Stack
       px="6"
@@ -23,7 +25,7 @@ export default function BlogPosts() {
           fontWeight="bold"
           _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
         >
-          How to create a cli with clack
+          {post.title}
         </panda.h3>
         <panda.small
           display="flex"
@@ -32,20 +34,18 @@ export default function BlogPosts() {
           color="neutral.500"
           textWrap="pretty"
         >
-          Learn how to create a beautiful CLI to generate projects.
+          {post.description}
         </panda.small>
         <Flex alignItems="center" gap="2" justifyContent="space-between" mt="4">
           <Box spaceX="3">
-            {['Tutorial', 'May 31, 2024'].map((content, index) => (
-              <Tag key={index}>
-                <HStack gap="1">
-                  <TagIcon />
-                  <panda.span>{content}</panda.span>
-                </HStack>
-              </Tag>
-            ))}
+            <Tag>
+              <HStack gap="1">
+                <TagIcon />
+                <panda.span>{format(post.date)}</panda.span>
+              </HStack>
+            </Tag>
           </Box>
-          <Link href="/">
+          <Link href={post.url}>
             <HStack
               gap="2"
               _hover={{

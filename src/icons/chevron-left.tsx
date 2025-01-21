a8a33cedd05737a48d57f2132453c403c8c90fd1
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-21 14:58:58 -0500

    Increase size of ChevronLeft icon to 1.3em

--- src/icons/chevron-left.tsx
+++ src/icons/chevron-left.tsx
@@ -3,8 +3,8 @@ import type { ComponentProps, FC } from 'react'
 const ChevronLeft: FC<ComponentProps<'svg'>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
+    width="1.3em"
+    height="1.3em"
     viewBox="0 0 24 24"
     {...props}
   >

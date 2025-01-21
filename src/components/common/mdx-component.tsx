354b60447d4ccf9c00fb7b98a889640703af3690
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-21 14:59:03 -0500

    Add custom components support for MDXContent

--- src/components/common/mdx-component.tsx
+++ src/components/common/mdx-component.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import { useMDXComponent } from 'next-contentlayer2/hooks'
+import { MDXComponents } from 'mdx/types'
 
 interface MDXContentProps {
   code: string
 }
 
+const components = {
+  h1: (props: React.HTMLAttributes<HTMLHeadingElement>) => <h1 {...props} />,
+}
+
 export function MDXContent({ code }: MDXContentProps) {
   const Component = useMDXComponent(code)
-  return <Component />
+  return <Component components={components as MDXComponents} />
 }

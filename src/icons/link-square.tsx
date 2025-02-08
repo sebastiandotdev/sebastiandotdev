14d9aff079974958bbb50bdc250b2c13650bbab3
sebastiandotdev <sebastiandotdev@gmail.com>
2025-02-08 17:49:05 -0500

    Add LinkSquare icon and include project URLs

---
+++ src/icons/link-square.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from 'react'
+
+export default function LinkSquare(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M15 3h6v6m-11 5L21 3m-3 10v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+      ></path>
+    </svg>
+  )
+}

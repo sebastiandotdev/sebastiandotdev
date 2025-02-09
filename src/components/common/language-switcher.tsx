cd9d2ec35a857510520ab131026e7a19e8aa0b36
sebastiandotdev <sebastiandotdev@gmail.com>
2025-02-09 18:33:56 -0500

    Improve UI `languague-switcher` with Tooltip

--- src/components/common/language-switcher.tsx
+++ src/components/common/language-switcher.tsx
@@ -4,6 +4,13 @@ import { usePathname, useRouter } from 'next/navigation'
 import { USIcon, COIcon } from '@/src/icons'
 import { css } from '@/styled-system/css'
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/src/components/ui/tooltip'
+
 export default function LanguageSwitcher() {
   const pathname = usePathname()
   const router = useRouter()
@@ -17,17 +24,26 @@ export default function LanguageSwitcher() {
   }
 
   return (
-    <button
-      onClick={toggleLanguage}
-      className={css({
-        cursor: 'pointer',
-      })}
-    >
-      {currentLocale === 'en' ? (
-        <COIcon className={css({ fontSize: '3xl' })} />
-      ) : (
-        <USIcon className={css({ fontSize: '3xl' })} />
-      )}
-    </button>
+    <TooltipProvider delayDuration={100}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <button
+            onClick={toggleLanguage}
+            className={css({
+              cursor: 'pointer',
+            })}
+          >
+            {currentLocale === 'en' ? (
+              <COIcon className={css({ fontSize: '3xl' })} />
+            ) : (
+              <USIcon className={css({ fontSize: '3xl' })} />
+            )}
+          </button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{currentLocale === 'en' ? 'Español' : 'Inglés'}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }

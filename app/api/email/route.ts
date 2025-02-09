ee28ef0d74fa3e34929fa00211bb44f31ac50572
sebastiandotdev <sebastiandotdev@gmail.com>
2025-02-08 22:30:36 -0500

    Refactor email authentication to use destructured variables

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -11,8 +11,8 @@ export async function POST(req: Request) {
     port: 465,
     secure: true,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user,
+      pass,
     },
     tls: {
       rejectUnauthorized: false,
